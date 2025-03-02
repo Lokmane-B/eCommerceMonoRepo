ad9c5926821613773f29333ccb6c44ed0b7937b3
CHASSAING <chassaing.a@outlook.fr>
2025-03-02 15:58:26 +0100

    feat: init ecom backend

---
+++ apps/ecom-backend/src/test/kotlin/fr/dev/ecom/EcomBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package fr.dev.ecom
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EcomBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
